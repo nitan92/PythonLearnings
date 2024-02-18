c92acd14121ae0f9bd1940427122193c4914c29d
Nitan Rana <149224006+nitan92@users.noreply.github.com>
2024-02-18 14:12:08 +0530

    Add files via upload

---
+++ 07_Feb_2024/Lab024.py
@@ -0,0 +1,10 @@
+# escaped character sequence
+
+name = "Nitan"
+newline_char = '\n'
+newline_char2 = '\t'
+newline_char3 = '\a'
+surname = 'Rana'
+print("Tab: ",name,"\t",surname)
+print("Newline: ",name,"\n",surname)
+print("Control Character BEL: ",name,"\a",surname)
