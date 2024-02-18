801ca040161f6753f2b5803618965b518d457744
Nitan Rana <149224006+nitan92@users.noreply.github.com>
2024-02-18 13:48:54 +0530

    Add files via upload

---
+++ 05_Feb_2024/Lab014.py
@@ -0,0 +1,22 @@
+# Strings
+# Bunch of Char
+# "" or ''
+name = "Nitan"
+name2 = 'Rana'
+print(name)
+print(name2)
+print(type(name))
+print(type(name2))
+
+# raw String
+# This will be helpful in the dir paths
+dir = r'C:\nomedir\aome dir'
+print(dir)
+
+
+# format String
+first_name = "Atul"
+last_name = "Kumar"
+age = 34
+isMarried = False
+print(f'Your name is {first_name} {last_name}, you age is {age} and you are {isMarried} ')
